57e2221d5a21e695a997ad839e499fb0d10e0e33
phosphotungstic <phosphotungstic@gmail.com>
2018-05-10 12:58:02 -0400

    adding more login stuff. an get post from express now yay

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 const assert = require('assert');
+var bodyParser = require('body-parser');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//pages
 app.get('/', home);
+
+//api
+app.post('/api/login', login);
+
+
 app.listen(3000, () => console.log('Example app listening on port 3000'));
 
 function home(req, res) {
   res.sendFile(__dirname + '/pages/index.html');
+}
+
+function login(req, res) {
+  console.log(req.body);
+  res.send('yay'); //switch to res.redirect
 }
--- public/js/controllers/login-form.controller.js
+++ public/js/controllers/login-form.controller.js
@@ -1,6 +1,14 @@
 angular.module('ketoboy')
   .controller('LoginFormController', LoginFormController);
 
-function LoginFormController() {
-  
+LoginFormController.$inject = ['$http'];
+
+function LoginFormController($http) {
+  var ctrl = this;
+
+  ctrl.submit = function(user) {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    $http.post('/api/login', user, {headers: headers});
+  }
 }
