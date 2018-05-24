4900f368aa106053373c5ead09d332e16c9fcc21
phosphotungstic <phosphotungstic@gmail.com>
2018-05-24 01:14:05 -0400

    added semicolon

--- db_scripts/create_tables.sql
+++ db_scripts/create_tables.sql
@@ -2,15 +2,15 @@
   create_tables.sql - creates all tables for ketoboy
 */
 
-create table user(
+create table if not exists user(
   user_id INTEGER PRIMARY KEY AUTOINCREMENT,
   username CHAR(20) NOT NULL,
   password CHAR(20) NOT NULL
 );
 
-create table calorie(
+create table if not exists calorie(
   calorie_id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER NOT NULL,
   calorie INTEGER NOT NULL,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-)
+);
