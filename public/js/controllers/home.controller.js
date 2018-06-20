f0d45a7c019c8fa81ae78978430d22519663c23a
phosphotungstic <phosphotungstic@gmail.com>
2018-06-20 01:00:02 -0400

    adding some more stuff just so I have notes on what I want to add later. max bars, legends, etc.

--- public/js/controllers/home.controller.js
+++ public/js/controllers/home.controller.js
@@ -5,7 +5,9 @@ function HomeController($scope) {
   var ctrl = this;
   
   ctrl.dailyLabels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-  ctrl.calorieData = [65, 59, 80, 81, 56, 55, 40];
+  ctrl.calorieData = [
+    [65, 59, 200, 81, 56, 55, 40],
+    [100, 100, 100, 100, 100, 100, 100]];
   ctrl.weightData = [265, 259, 280, 281, 256, 255, 240];
 
   ctrl.calorieOptions = 
@@ -16,11 +18,29 @@ function HomeController($scope) {
                 beginAtZero:true
             }
         }]
+    },
+    legend: {
+      display: true,
+      position: "right"
     }
-  }
+  };
+
+  ctrl.calorieOverride =
+  [{
+    label: "Calories",
+    borderWidth: 1,
+    type: 'bar',
+  },
+  {
+    label: "Max Calorie Limit",
+        borderWidth: 3,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        type: 'line'
+  }];
 
   ctrl.weightOverride = 
   {
     lineTension: 0
-  }
+  };
 }
